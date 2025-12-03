6a59a84b6712be4186677be318f57eec156cbf13
Pranav P <pranavp.work@gmail.com>
2025-12-03 22:28:25 +0530

    sending image using path express & sendFile in res object

--- nodejs/PROJECTS/express-project/controllers/messages.controller.js
+++ nodejs/PROJECTS/express-project/controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hello, world!<li><ul>')
+    // res.send('<ul><li>Hello, world!<li><ul>')
+    path.join(__dirname, '..', 'public', 'skimountain.jpg');
+    res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
 }
 
 function postMessage(req, res) {
