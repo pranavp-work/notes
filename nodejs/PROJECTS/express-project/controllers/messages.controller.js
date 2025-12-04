3f38ba7de7e496e2caaf81b3232e538be7d1f985
Pranav P <pranavp.work@gmail.com>
2025-12-04 19:37:30 +0530

    handlebars template engine added

--- nodejs/PROJECTS/express-project/controllers/messages.controller.js
+++ nodejs/PROJECTS/express-project/controllers/messages.controller.js
@@ -3,7 +3,11 @@ const path = require('path');
 function getMessages(req, res) {
     // res.send('<ul><li>Hello, world!<li><ul>')
     path.join(__dirname, '..', 'public', 'skimountain.jpg');
-    res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+    // res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+    res.render('messages', {
+        title: 'Messages Page',
+        friend: 'Pranav',
+    })
 }
 
 function postMessage(req, res) {
